d4c1d7c168a72b4566a4c0fdbde9f0167bddda52
adityasharma2020 <aditya15116617@gmail.com>
2023-11-21 18:30:31 +0530

    navbar responsiveness and sticky

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const toggleBtn = document.getElementById('toggleBtn');
 const dropDownLinks = document.getElementById('dropdown');
+const navbar = document.getElementById('navbar');
+const logo = document.getElementById('logo');
 
 toggleBtn.onclick = function () {
 	dropDownLinks.classList.toggle('open');
 };
+
+window.addEventListener('scroll', () => {
+	if (navbar && window.scrollY > 100) {
+		navbar.classList.add('scroll');
+		logo.classList.add('logoScroll');
+	} else {
+		navbar.classList.remove('scroll');
+		logo.classList.remove('logoScroll');
+	}
+});
